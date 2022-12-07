6adee8294312e549d64bdfde72a3be6abf67bd3c
Milo <milo.gianinazzi@gmail.com>
2022-12-07 20:51:12 +0100

    Fixed events

--- app/src/main/java/ch/milog/kavavin_remastered/presentation/add_bottle/AddBottleEvent.kt
+++ app/src/main/java/ch/milog/kavavin_remastered/presentation/add_bottle/AddBottleEvent.kt
@@ -2,4 +2,13 @@ package ch.milog.kavavin_remastered.presentation.add_bottle
 
 sealed class AddBottleEvent {
     object AddBottle : AddBottleEvent()
+    data class NameChanged(val name: String) : AddBottleEvent()
+    data class YearChanged(val year: String) : AddBottleEvent()
+    data class CountryChanged(val country: String) : AddBottleEvent()
+    data class RegionChanged(val region: String) : AddBottleEvent()
+    data class GrapesChanged(val grapes: String) : AddBottleEvent()
+    data class ProducerChanged(val producer: String) : AddBottleEvent()
+    data class TypeChanged(val type: String) : AddBottleEvent()
+    data class QuantityChanged(val quantity: String) : AddBottleEvent()
+    data class PriceChanged(val price: String) : AddBottleEvent()
 }
